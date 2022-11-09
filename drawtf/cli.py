6ecbe6316ca6e4ca9872cb033b69ab6c07b2a15d
Michael Law <Michael.Law@aggreko.com>
2022-11-09 09:56:41 +0000

    State and path inm config too

--- drawtf/cli.py
+++ drawtf/cli.py
@@ -18,20 +18,32 @@ UNPARENTED = "Unparented"
 @click.option('--platform', help="The platform to use 'azure' or 'aws', only 'azure' currently supported",  default='azure')
 @click.option('--output-path', help='Output path if to debug generated json populated.')
 @click.option('--json-config-path', help='Config file path if populated.')
-@click.option('--json-output-path', help='Output path if to debug generated json populated.')
 @click.option('--verbose', is_flag=True, default=False, help='Add verbose logs.')
-def main(name: str, state: str, platform: str, output_path: str, json_config_path: str, json_output_path: str, verbose: bool):
+def main(name: str, state: str, platform: str, output_path: str, json_config_path: str, verbose: bool):
     """Console script for drawtf."""
+    # Set logging level
+    if verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
+        
+    # Load the config if it exists
+    config_data = None
+    if (not json_config_path is None):
+        logging.info(f"Reading config from {json_config_path}.")
+        
+        if (not os.path.exists(json_config_path)):
+            raise click.BadParameter(
+                message="The path to the config file does not exist.")
+            
+        cf = open(json_config_path)
+        config_data = json.load(cf)
+
+    # Get the state file passed in or in config and set it up
+    if (not config_data == None and "state" in config_data and state == None):
+        state = config_data["state"]
+        
     if (not state == None and not os.path.exists(state)):
         raise click.BadParameter(
             message="The path to the state file does not exist.")
-
-    if (not json_config_path == None and not os.path.exists(json_config_path)):
-        raise click.BadParameter(
-            message="The path to the config file does not exist.")
-
-    if verbose:
-        logging.getLogger().setLevel(logging.DEBUG)
 
     if (not state == None):
         f = open(state)
@@ -40,15 +52,27 @@ def main(name: str, state: str, platform: str, output_path: str, json_config_pat
         data = {
             "resources": []
         }
-
-    components: List[Component] = []
-    resources = {}
+        
+    # Validate name and output path too
+    if (not config_data == None and name == None and "name" in config_data):
+        name = config_data["name"]
+        
+    if (name == None):
+        name = "Design"
+        
+    if (not config_data == None and output_path == None and "outputPath" in config_data):
+        output_path = config_data["outputPath"]
+        
+    if (output_path == None and not json_config_path == None):
+        output_path = os.path.splitext(json_config_path)[0]
 
     supported_nodes = []
-    if (platform == 'azure'):
+    if (platform.lower() == 'azure'):
         supported_nodes = azure.supported_nodes()
     else:
-        logging.error(f"Platform {platform} is not yet supported.")
+        raise Exception(f"Platform {platform} is not yet supported.")
+
+    components: List[Component] = []
 
     for resource in data["resources"]:
         resource_name = resource["name"]
@@ -70,21 +94,11 @@ def main(name: str, state: str, platform: str, output_path: str, json_config_pat
                 resource_name = attributes["name"]
 
             print(f"Adding resource {resource_name}-{type}")
-            resources[resource_name] = {}
-            resources[resource_name]["name"] = resource_name
-            resources[resource_name]["type"] = type
-            resources[resource_name]["mode"] = mode
-            resources[resource_name]["resource_group"] = resource_group_name
-            resources[resource_name]["attributes"] = attributes
-            resources[resource_name]["components"] = {}
             components.append(Component(resource_name, type,
                               mode, resource_group_name, attributes))
 
     links = None
-    if (not json_config_path is None):
-        logging.info(f"Reading config from {json_config_path}.")
-        cf = open(json_config_path)
-        config_data = json.load(cf)
+    if (not config_data is None):
         if "links" in config_data:
             links = config_data["links"]
         if "components" in config_data:
@@ -98,31 +112,13 @@ def main(name: str, state: str, platform: str, output_path: str, json_config_pat
                 attributes = instance["attributes"]
                 
                 print(f"Adding resource (from config) {resource_name}-{type}")
-                resources[resource_name] = {}
-                resources[resource_name]["name"] = resource_name
-                resources[resource_name]["type"] = type
-                resources[resource_name]["mode"] = mode
-                resources[resource_name]["resource_group"] = resource_group_name
-                resources[resource_name]["attributes"] = attributes
-                resources[resource_name]["components"] = {}
                 components.append(Component(resource_name, type,
                               mode, resource_group_name, attributes))
-
-    if (not json_output_path is None):
-        logging.info(f"Writing debug output to {json_output_path}.")
-        with open(json_output_path, 'w') as f:
-            json.dump(resources, f, indent=4)
-
-    if (platform == 'azure'):
-        if (name == None and "name" in config_data):
-            name = config_data["name"]
             
-        if (name == None):
-            name = "Design"
-            
+    if (platform.lower() == 'azure'): 
         azure.draw(name, output_path, components, links)
     else:
-        print(f"Platform {platform} is not yet supported.")
+        raise Exception(f"Platform {platform} is not yet supported.")
 
     return 0
 
